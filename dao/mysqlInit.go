29c4b571a4e998eb4a012cdead76887dea16ed59
Mxxxx13 <1623422215@qq.com>
2022-04-20 15:40:43 +0800

    feat: gorm AutoMigrate

--- dao/mysqlInit.go
+++ dao/mysqlInit.go
@@ -22,6 +22,6 @@ func ConnDB() {
 	db.SingularTable(true)      // 表名为单数
 	db.DB().SetMaxIdleConns(10) // 最大活跃连接数
 	db.DB().SetMaxOpenConns(20) // 最大空闲连接数
-	db.AutoMigrate(&model.User{}, &model.Student{})
+	db.AutoMigrate(&model.User{}, &model.Student{}, &model.Course{}, &model.CourseSelection{}, &model.Teacher{})
 	DB = db
 }
